a98b57c20a115f012b5d5df3e2323c443890c3d0
Ido Nov <idonov8@gmail.com>
2021-07-21 11:38:20 +0300

    Update client/src/app/store.js
    I hope it works lol
    
    Co-authored-by: omer1BS <43821786+omer1BS@users.noreply.github.com>

--- client/src/app/store.js
+++ client/src/app/store.js
@@ -2,4 +2,4 @@ import { createStore, applyMiddleware, compose } from '@reduxjs/toolkit'
 import rootReducer from '../reducers/index'
 import thunk from 'redux-thunk'
 
-export const store = createStore(rootReducer, compose(applyMiddleware(thunk), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()))
+export const store = createStore(rootReducer, compose(applyMiddleware(thunk), window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : f=>f ))
